a5b15095f381963d9ad47886a3cca53ef734cbc6
Suma NB <Sumabhaskar@2016>
2024-07-07 18:05:12 +0530

    1234|feature: test_intelliji

--- testintelliji.java
+++ testintelliji.java
@@ -2,5 +2,6 @@ public class testintelliji {
 
     public static void main(String[] args) {
         System.out.println("suma is good");
+        System.out.println("try commit 2 via branch");
     }
 }
